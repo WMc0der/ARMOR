a199d2e71863bb6e52bf394f90f4a1145a9fe038
William Mank <79393090+WMc0der@users.noreply.github.com>
2025-07-31 20:49:19 -0500

    Add files via upload
    Added files on ARMOR from computer. Initial thoughts and ideas.

---
+++ ARMOR_Main.swift
@@ -0,0 +1,104 @@
+//ARMOR v0.1
+//Created by: William Mank
+//Company: WM Developments
+//Start Date: 04/06/2021
+//First Level Prototype by 07/04/2021
+
+/* CHECKLIST
+	[ ] Levels:
+		[ ] 1. Start on Ship Attack and make your way to escape pod.
+		[ ] 2. Crash land on Alien planet, need to repair ship parts
+		[ ] 3. Meet Secondary Character <Can unlock and interchange>
+		[ ] 4. Blah
+		[ ] 5. Learn Ant Colony is heading to Earth to colonize and terraform into their planet
+		[ ] 6. Blah
+		[ ] 7. Blah
+		[ ] 8. Back on Earth, and save a small city.
+		[ ] 9. Blah
+		[ ] 10. Make your way through Ant Colony and destroy the Queen Ant
+	
+	[ ] Characters:
+		[ ] 1. Choose Gender
+		[ ] 2. Choose Race
+		[ ] 3. Choose starting armor class
+		[ ] 4. Choose basic colors
+	
+	[ ] UI
+		[ ] 1. Main Menu
+			[ ] i. New Game
+			[ ] ii. Continue Game (How to save data?)
+			[ ] iii. Options
+		[ ] 2. Logic choice between choices in I -> iii
+			[ ] i. Choice New Game
+				[ ] 1) Character Set-Up
+					[ ] a) OPEN <CHARACTERS>
+				[ ] 2) Weapons and Armor Set-Up
+					[ ] a) OPEN <WEAPONS>
+					[ ] b) OPEN <ARMOR>
+				[ ] 3) Level 
+					[ ] a) Start level 1
+			[ ] ii. Continue Game
+				[ ] 1) Load Out Screen
+					[ ] a) Choose character to continue playing
+					[ ] b) Choose 1/2 choices:
+						[ ] i) Load Game
+						[ ] ii) Load Armory
+			[ ] iii. Options
+				[ ] 1) Difficulty
+				[ ] 2) Volume
+					[ ] a) Music
+					[ ] b) sFx
+				[ ] 3) Graphics
+Credits
+*/
+
+//PROTOTYPE --> Use shapes to define characters, get basic movements and menus down
+
+//UI
+//PSUEDO CODE (take from notebook)
+    /*
+	UI
+		1. Main Menu
+            - load background image.
+            - load title
+                - FONT
+                - SIZE
+                - COLOR
+            -SELECT between three main choices:
+                IF/ELSE STATEMENT (Maybe LAMBDA CHOICES?)
+                    - NEW GAME
+                        - CENTER | ICON | ANIMATION?
+                    - OPTIONS
+                        - LEFT | ICON | ANIMATION?
+                    - CONTINUE
+                        - RIGHT | ICON | ANIMATION?
+			i. New Game
+                - <STANDARD IMAGES LOADED>
+                - <STANDARD TEXT NECESARRY LAODED>
+                
+			ii. Continue Game (How to save data?)
+			iii. Options
+		2. Logic choice between choices in I -> iii
+			i. Choice New Game
+				1) Character Set-Up
+					a) OPEN <CHARACTERS>
+				2) Weapons and Armor Set-Up
+					a) OPEN <WEAPONS>
+					b) OPEN <ARMOR>
+				3) Level 
+					a) Start level 1
+			ii. Continue Game
+				1) Load Out Screen
+					a) Choose character to continue playing
+					b) Choose 1/2 choices:
+						i) Load Game
+						ii) Load Armory
+			iii. Options
+				1) Difficulty
+				2) Volume
+					a) Music
+					b) sFx
+				3) Graphics
+
+
+    */
